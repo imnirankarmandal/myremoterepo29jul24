727866ad1e8e136a024bf3100949a442494ad02b
Nirankar <niranakr.d.mandal@gmail.com>
2024-07-30 05:03:38 +0530

    apply git stash

--- myjavafile.java
+++ myjavafile.java
@@ -1,2 +1,10 @@
 This is java file
+<<<<<<< Updated upstream
 manager work completed
+=======
+1st line of java code completed
+2nd line of java code completed
+3rd line of java code completed
+4th line of java code completed
+5th line of java code completed
+>>>>>>> Stashed changes
